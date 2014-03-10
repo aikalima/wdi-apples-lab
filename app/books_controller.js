4ffa3eb07cf1beeba95d72e3b200e140caa5ca26
markusg <mguehrs@gmail.com>
2014-03-09 18:46:54 -0700

    bunus feature

--- app/books_controller.js
+++ app/books_controller.js
@@ -10,19 +10,29 @@ bookly.BooksController = function($scope) {
   $scope.cart = [];
 
   $scope.addToCart = function(aBook) {
-    $scope.cart.push(aBook);
-  }
+    var itemInCart = _.findWhere($scope.cart, {book: aBook});
+    if (itemInCart != undefined){
+      var index = _.indexOf($scope.cart, itemInCart);
+      $scope.cart[index].count = $scope.cart[index].count + 1;
+    } else {
+      var cart_item = {
+        book: aBook,
+        count: 1
+      };
+      $scope.cart.push(cart_item);
+    }
+  };
 
   $scope.clearCart = function() {
     $scope.cart = [];
-  }
+  };
 
   $scope.cartTotal = function() {
     var total = 0.0;
-    angular.forEach($scope.cart, function(item, key){
-      total += item.price;
+    _.each($scope.cart, function(cart_item, key){
+      total += cart_item.book.price * cart_item.count;
     });
     return total;
-  }
+  };
 
 };
